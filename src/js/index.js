a5c853147fb16078a93ae405b07159627ffec537
Moiseis-Cruz <moizzito@gmail.com>
2023-02-01 12:26:31 -0300

    Colocando obrigatoriedade no campo do nome.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-function regirter(){
+/*function regirter(){
     let name = document.getElementById("name")
 
     let lastName = document.getElementById("last-name")
@@ -7,5 +7,60 @@ function regirter(){
 
     let password = document.getElementById("password")
 
+    let formulary = document.getElementById("form")
+
+    formulary.addEventListener('submit', (e) => {
+        e.preventDefault()
     
+        checkInputs()
+    })
+}*/
+
+
+
+let formulary = document.getElementById("form")
+
+let userName = document.getElementById("name")
+
+ let lastName = document.getElementById("last-name")
+
+let email = document.getElementById("email")
+
+let password = document.getElementById("password")
+
+formulary.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    checkInputs()
+})
+
+
+function checkInputs() {
+    const nameValue = userName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+
+    if(nameValue === ""){
+        errorValidation(userName, "Isso é um teste")
+    }else{
+        successValidation(userName)
+    }
+
+    
+}
+
+function errorValidation(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small")
+
+    small.innerText = message
+
+    formControl.className = "form-control error"
+}
+
+function successValidation(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = "form-control success"
 }
