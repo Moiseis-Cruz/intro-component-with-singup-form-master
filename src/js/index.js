6197bb860568303c2896372a0fafa876e813b760
Moiseis-Cruz <moizzito@gmail.com>
2023-02-13 19:31:32 -0300

    Refiz o código inteiro.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-let formulary = document.getElementById("form")
+/*let formulary = document.getElementById("form")
 
 let userName = document.getElementById("name")
 
- let lastName = document.getElementById("last-name")
+let lastName = document.getElementById("last-name")
 
 let email = document.getElementById("email")
 
@@ -14,6 +14,77 @@ formulary.addEventListener('submit', (e) => {
     checkInputs()
 })
 
+
+function checkInputs() {
+    const nameValue = userName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+
+    if(nameValue === ""){
+        errorValidation(userName, "First Name cannot be empty")
+    }else{
+        successValidation(userName)
+    }
+
+    if(lastNameValue === ""){
+        errorValidation(lastName, "Last Name cannot be empty")
+    }else{
+        successValidation(lastName)
+    }
+
+    if(emailValue === ""){
+        errorValidation(email, "Looks like this is not an email")
+    }else{
+        successValidation(email)
+    }
+
+    if(passwordValue === ""){
+        errorValidation(password, "Password cannot be empty")
+    }else{
+        successValidation(password)
+    }
+}
+
+function errorValidation(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small")
+
+    small.innerText = message
+
+    formControl.className = "form-control error"
+}
+
+function successValidation(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = "form-control success"
+}*/
+
+
+
+
+
+
+
+
+
+
+let enviarFomrulario = document.querySelector("button[class='btn-claim']")
+
+let userName = document.getElementById("name");
+
+let lastName = document.getElementById("last-name");
+
+let email = document.getElementById("email");
+
+let password = document.getElementById("password");
+
+enviarFomrulario.addEventListener("click", function(e){
+    e.preventDefault()
+
+    checkInputs()
+})
 
 function checkInputs() {
     const nameValue = userName.value.trim()
