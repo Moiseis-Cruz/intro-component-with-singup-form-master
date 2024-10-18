a7f2986efa87e7674871918f620fa2ecd20214c9
Moiseis Cruz <moizzito@gmail.com>
2024-10-17 21:11:43 -0300

    Deixando o código mais organizado.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
-let formControl = document.querySelectorAll(".form-control")
+let formControl = document.querySelectorAll(".form-control");
 
-let input = document.querySelectorAll("input")
+let input = document.querySelectorAll("input");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault();
     
     input.forEach((input, i) => {
         if(input.value === ""){
-            formControl[i].classList.add("error")
+            formControl[i].classList.add("error");
         }else{
-            formControl[i].classList.remove("error")
-        }
-    })
+            formControl[i].classList.remove("error");
+        };
+    });
     
-})
+});
